6655e76e0971ba761e91319d112c5c89b69e535d
franck237 <franck.kamanou@gmail.com>
2019-07-22 18:42:18 +0200

    Ajout des 2 premiers exercices

--- lib/event.rb
+++ lib/event.rb
@@ -1,3 +1,64 @@
+require 'pry'
+require 'time'
+
 class Event
+	attr_accessor :start_date, :start_date_string, :duration, :title, :attendees
+	
+
+	def initialize(start_date_string, duration, title, attendees_list)
+		@start_date_string = start_date_string
+		@start_date = Time.parse(start_date_string)
+		@duration = duration.to_i
+		@title = title
+		@attendees = attendees_list
+		return start_date
+
+	end
+
+	def postpone_24h
+		postpone_date = start_date + (24*3600)
+		@start_date = postpone_date
+		return start_date	
+	end
+
+
+	def show_start_date
+		puts start_date
+	end
+
+	def show_duration_class
+		puts duration.class	
+	end
+
+	def end_date
+		end_date = start_date + duration*60
+		return end_date
+	end
+
+	def is_past?
+		start_date < Time.now
+	end
+
+	def is_future?
+		!is_past?
+	end
+
+	def is_soon?
+		Time.now < start_date < Time.now + (30*60)
+	end
+
+	def to_s
+		puts " >Titre : #{@title}"
+		puts ">Date de début : #{start_date.strftime("%Y-%m-%d %H:%M")}"
+		puts ">Durée : #{duration} minutes"
+		print ">Invités : "
+		attendees.each do |i| 
+			print i + ", "
+		end
+		return
+	end
 
 end
+
+binding.pry
+puts "end of file"
--- lib/user.rb
+++ lib/user.rb
@@ -1,3 +1,41 @@
+require 'pry'
+
 class User
+	attr_accessor :email, :age
+	@@user_list = []
+
+	def initialize(email,age)
+		@email = email
+		@@user_list << self
+		@age = age.to_i
+	end
+
+	#def change_password(new_password)
+	#	@encrypted_password = encrypt(new_password)
+	#end
+
+	def show_itself
+		puts self
+	end
+
+	def self.all
+		return @@user_list
+	end
+
+	def self.find_by_email(email_search)
+		@@user_list.each do |user|
+			if user.email == email_search
+			puts "Voici l'age du User trouvé : #{user.age}"
+			end
+		end	
+	end
+	#private
+
+	# def encrypt(string_to_encrypt)
+   # return "##ENCRYPTED##"
+  	#end
 
 end
+
+binding.pry
+puts "end of file"
